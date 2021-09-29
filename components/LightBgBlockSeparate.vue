b8f14054e14441bee642e578c63b08ac4c41f498
Евгений Гусев <evgenii.gusev@outofcloud.ru>
2021-09-29 20:58:19 +0300

    first commit

---
+++ components/LightBgBlockSeparate.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>mock</div>
+</template>
+
+<script>
+export default {
+  name: 'fake'
+};
+</script>
---
+++ index.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<component is="someComponentFromImports"/>
+
+    <main-header />
+    <dark-bg-blockg />
+
+		<div id="extract_me">
+			<div>
+				mock
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+import ImageTitleList from './components/ImageTitleList.vue';
+import DarkBgBlock from './components/DarkBgBlock.vue';
+import TextAlign from './components/TextAlign.vue';
+import ImageFullWidth from './components/ImageFullWidth.vue';
+import ThreeQuartersWidth from './components/ThreeQuartersWidth.vue';
+import GoalsBlock from './components/GoalsBlock.vue';
+import BlockTitle from './components/BlockTitle.vue';
+import ScoresBlock from './components/ScoresBlock.vue';
+import UnnumberedList from './components/UnnumberedList.vue';
+import NumberedList from './components/NumberedList.vue';
+import LogoBlock from './components/LogoBlock.vue';
+import TeamBlock from './components/TeamBlock.vue';
+import FlexImageTripple from './components/FlexImageTripple.vue';
+import FlexDouble from './components/FlexDouble.vue';
+
+export default {
+	components: {
+		TeamBlock,
+		BlockTitle,
+		GoalsBlock,
+		TextAlign,
+		ImageFullWidth,
+		ThreeQuartersWidth,
+		MainHeader,
+		DarkBgBlock,
+		ScoresBlock,
+		ImageTitleList,
+		UnnumberedList,
+		NumberedList,
+		LogoBlock,
+		FlexImageTripple,
+		FlexDouble
+	},
+};
+</script>
